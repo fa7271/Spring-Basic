34c6952fef5d1adabfe74e23aff8e4df95af6e46
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-06-19 22:41:20 +0900

    06/19

--- src/main/java/Hello/core/member/MemberRepository.java
+++ src/main/java/Hello/core/member/MemberRepository.java
@@ -2,7 +2,7 @@ package Hello.core.member;
 
 public interface MemberRepository { //인터페이스 안건드림
 
-    void save(Member member);   //멤버 회원을 저장
+    void save(Member member);   //멤버 회원을 저장 void 는 리턴값이 없음 헷갈리지 말기.
     Member findBId(Long memberId);// 회원의 아이디를 찾는기능
 }
 
